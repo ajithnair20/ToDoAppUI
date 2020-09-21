f62450c3b0c98be4e13a77dea72642e4ad2270af
Ajith <ajith.jayan20@gmail.com>
2020-09-21 11:55:36 -0500

    initial commit

---
+++ todoappui/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
+)
+
+
+
+// The application has been created using the reference code at https://github.com/ibrahima92/fullstack-typescript-mern-todo
